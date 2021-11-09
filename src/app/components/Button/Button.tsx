55d1d524ac42b267648ce2a59aa6051fe38664a7
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-09 16:04:23 +0100

    Create StyledButton with styled components

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,12 +1,21 @@
 import type { ReactNode } from 'react';
 import React from 'react';
-import classes from './Button.module.css';
+import styles from 'styled-components';
 
 type ButtonProps = {
   children: ReactNode;
 };
 function Button({ children }: ButtonProps): JSX.Element {
-  return <button className={classes.button}>{children}</button>;
+  return <StyledButton>{children}</StyledButton>;
 }
 
 export default Button;
+
+const StyledButton = styles.button`
+border-radius: 50%;
+border: 1px solid orange;
+background-color: green;
+color: white;
+padding: 0.8rem 2rem;
+text-transform: lowercase;
+`;
