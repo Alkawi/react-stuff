da41c60fea43b585180cb6d0f8465d7af184910c
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-10 11:50:00 +0100

    Rename Tag component files to Tags

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Tags from '../Tag/Tag';
+import Tags from '../Tags/Tags';
 
 type CardProps = {
   title: string;
--- src/app/components/Tags/Tags.stories.tsx
+++ src/app/components/Tags/Tags.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tags from './Tag';
+import Tags from './Tags';
 
 export default {
   title: 'Component/Tag',
