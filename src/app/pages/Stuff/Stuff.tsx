8eb7f0185bb5b2d077689dfde347d163aa7e7ab8
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-11 12:27:45 +0100

    Implement useEffect to fetch one thing in Stuff

--- src/app/pages/Stuff/Stuff.tsx
+++ src/app/pages/Stuff/Stuff.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import DetailCard from '../../components/DetailCard/DetailCard';
+import type { Thing } from '../../types';
 
 function Stuff(): JSX.Element {
-  const params = useParams();
-  return <h2>{params.stuffs}</h2>;
+  const thingParams = useParams();
+  const [thing, setThing] = useState<Thing | null>();
+
+  useEffect(() => {
+    async function fetchThing() {
+      const response = await fetch(
+        `https://json-server.neuefische.de/stuff/${thingParams.stuffs}`
+      );
+      const fetchedThing = await response.json();
+      setThing(fetchedThing);
+    }
+    fetchThing();
+  }, []);
+  return (
+    <div>
+      {thing && (
+        <DetailCard
+          title={thing?.name}
+          description={thing?.description}
+          categories={thing?.categories}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Stuff;
