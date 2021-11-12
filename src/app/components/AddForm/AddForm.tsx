ff30bb924304c2ed7ae2da99b3c63aed408c55f1
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-12 14:30:16 +0100

    Add basic stucture

---
+++ src/app/components/AddForm/AddForm.tsx
@@ -0,0 +1,17 @@
+import { format } from 'path/posix';
+import React from 'react';
+
+export default function AddForm(): JSX.Element {
+  return (
+    <form action="">
+      <label>
+        <input type="text" />
+      </label>
+      <label>
+        <input type="text" />
+      </label>
+
+      <input type="submit" />
+    </form>
+  );
+}
