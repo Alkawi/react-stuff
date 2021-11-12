a73c02af45b996846c53e51d311ffa7f3fb545f7
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-12 12:20:57 +0100

    Remove useState & useEffect import from Dashboard & Stuff

--- src/app/pages/Stuff/Stuff.tsx
+++ src/app/pages/Stuff/Stuff.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import DetailCard from '../../components/DetailCard/DetailCard';
 import useFetch from '../../hooks/useFetch';
