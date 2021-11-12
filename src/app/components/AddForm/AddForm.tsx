490e60b3ae518ec6b34e8bcbfb8c0ddcaf2571bd
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-12 15:40:04 +0100

    Add styling to form

--- src/app/components/AddForm/AddForm.tsx
+++ src/app/components/AddForm/AddForm.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function AddForm(): JSX.Element {
   return (
-    <form action="">
-      <label>
-        <input type="text" />
-      </label>
-      <label>
-        <input type="text" />
-      </label>
+    <Form action="">
+      <Label>
+        Name: <input type="text" />
+      </Label>
+      <Label>
+        Description: <input type="text" />
+      </Label>
 
-      <input type="submit" />
-    </form>
+      <Button type="submit" />
+    </Form>
   );
 }
+
+const Form = styled.form`
+  display: grid;
+  row-gap: 10px;
+  background-color: blue;
+  color: #e2e2e2;
+  border-radius: 8px;
+  padding: 20px;
+`;
+
+const Button = styled.input`
+  border: 0;
+  border-radius: 8px;
+  background-color: lightblue;
+  color: #2e2e2e;
+  padding: 15px 20px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
