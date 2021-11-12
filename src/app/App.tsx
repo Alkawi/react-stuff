1395d6d09e148ab7e07184850ef7e1c93d00fe1f
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-12 15:40:28 +0100

    Add AddForm call in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddForm from './components/AddForm/AddForm';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Stuff from './pages/Stuff/Stuff';
 function App(): JSX.Element {
@@ -8,7 +9,7 @@ function App(): JSX.Element {
       navigation
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/app" element={<h1>Add Form</h1>} />
+        <Route path="/add" element={<AddForm />} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
 
