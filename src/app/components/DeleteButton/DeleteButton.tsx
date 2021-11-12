1e5cc7b55ea25949cdca10cb863a2f96ab0a47cc
Alexander Kaiser <75093518+Alkawi@users.noreply.github.com>
2021-11-12 17:21:28 +0100

    Add styled button

--- src/app/components/DeleteButton/DeleteButton.tsx
+++ src/app/components/DeleteButton/DeleteButton.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 export default function DeleteButton(): JSX.Element {
-  return <></>;
+  return <Button>x</Button>;
 }
+
+const Button = styled.button`
+  background-color: yellowgreen;
+  color: white;
+  border-radius: 7.34px;
+  border: none;
+  padding: 20px;
+`;
